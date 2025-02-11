538fead569cccb29fff565f1efa0db34303744de
justforjan <schneiderjan.2013@gmail.com>
2025-02-11 20:54:57 +0100

    change color

--- my-home-page/tailwind.config.js
+++ my-home-page/tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FE9900',
+        secondary: '#00BBA7'
+      }
+    },
   },
   plugins: [],
 }
