3bb6d3c14993d561bda27ba14e5335c5dbc325fb
Jan Schneider <jan.schneider@codecentric.de>
2025-03-07 20:38:51 +0100

    add main.py

---
+++ backend/main.py
@@ -0,0 +1,92 @@
+import os
+from contextlib import asynccontextmanager
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Depends
+from sqlmodel import SQLModel, create_engine, Session, select
+
+from app.models import Job, Project
+
+from app.data import jobs, projects
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "mywebsite")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        try:
+            yield session
+        finally:
+            session.close()
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+def fill_empty_tables_with_dummy_data(session: Session):
+    print('Filling empty tables with dummy data')
+    if not session.exec(select(Project)).first():
+        for project in projects:
+            print(f'Adding project {project.title}')
+            db_project = Project.model_validate(project)
+            session.add(db_project)
+        session.commit()
+
+    if not session.exec(select(Job)).first():
+        for job in jobs:
+            print(f'Adding job {job.title}')
+            db_job = Job.model_validate(job)
+            session.add(db_job)
+        session.commit()
+
+    print('Dummy data added to database')
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    fill_empty_tables_with_dummy_data(Session(engine))
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/jobs", response_model=list[Job])
+def get_all_jobs(session: SessionDep):
+    db_jobs = session.exec(select(Job))
+    return db_jobs
+
+
+@app.get('/jobs/{job_id}', response_model=Job)
+def get_job(job_id: int, session: SessionDep):
+    job = session.get(Job, job_id)
+    if not job:
+        raise HTTPException(status_code=404, detail=f'Job with the ID {job_id} does not exists')
+    return job
+
+
+@app.get('/projects', response_model=list[Project])
+def get_all_projects(session: SessionDep):
+    return session.exec(select(Project))
+
+
+@app.get('/projects/{project_id}', response_model=Project)
+def get_project_by_id(project_id: int, session: SessionDep):
+    project = session.get(Project, project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail=f'Project with the ID {project_id} does not exists')
+    return project
+
+
+
